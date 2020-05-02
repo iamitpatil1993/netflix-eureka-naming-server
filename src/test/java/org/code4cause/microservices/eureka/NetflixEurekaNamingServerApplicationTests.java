30d931d3cbbc299b978957c79d18e68bad5cf570
amipatil <iamitpatil1993@gmail.com>
2020-05-02 23:16:14 +0530

    Setup Eureka Namig server

---
+++ src/test/java/org/code4cause/microservices/eureka/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.code4cause.microservices.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
